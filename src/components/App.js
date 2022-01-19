520f14765ff488486f34ee361a89d2338c16a164
Tsimura <chappa2828@gmail.com>
2022-01-19 11:29:57 +0200

    fixed useLocalStorage.jsx

--- src/components/App.js
+++ src/components/App.js
@@ -8,16 +8,16 @@ import Filter from 'components/Filter/Filter';
 import { AppWrapper, WorkingSpace } from './App.styled';
 export default function App() {
   const [filter, setFilter] = useState('');
-  const [contacts, setContacts] = useLocalStorage('contacts', '');
+  const [contacts, setContacts] = useLocalStorage('contacts', [
+    { id: 'id-1', name: 'Rosie Simpson', number: '459-12-56' },
+    { id: 'id-2', name: 'Hermione Kline', number: '443-89-12' },
+    { id: 'id-3', name: 'Eden Clements', number: '645-17-79' },
+    { id: 'id-4', name: 'Annie Copeland', number: '227-91-26' },
+  ]);
   useEffect(() => {
     window.localStorage.setItem('contacts', JSON.stringify(contacts));
   }, [contacts]);
   const addContact = (name, number) => {
-    const contact = {
-      id: shortid.generate(),
-      name,
-      number,
-    };
     if (
       contacts.find(
         contact => contact.name.toLowerCase() === name.toLowerCase(),
@@ -25,6 +25,11 @@ export default function App() {
     ) {
       toast.error(`${name} is already in contacts.`);
     } else {
+      const contact = {
+        id: shortid.generate(),
+        name,
+        number,
+      };
       setContacts(prevContacts => [...prevContacts, contact]);
     }
   };
